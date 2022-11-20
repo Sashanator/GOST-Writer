00ba4d36e8d59a391b7a02b5ddb9012471cca194
Vitaliy Dikovitskiy <vdikovitsky@nntc.pro>
2022-11-20 20:12:01 +0300

    add gost setup

--- Application/Services/WordService.cs
+++ Application/Services/WordService.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reflection;
 using Microsoft.AspNetCore.Http;
+using Xceed.Document.NET;
 using Xceed.Words.NET;
 
 namespace Application.Services;
@@ -13,11 +14,29 @@ public class WordService : IWordService
         // Вернуть документ
         var ms = new MemoryStream();
         document.CopyTo(ms);
-        var doc = DocX.Load(ms);
-        doc.InsertParagraph("Hello!");
-        doc.Save();
-        var stream = typeof(DocX).GetField("_memoryStream", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(doc);
+        var wordDocument = DocX.Load(ms);
+        ApllyGOST(wordDocument);
+        var stream = typeof(DocX).GetField("_memoryStream", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(wordDocument);
 
         return Task.FromResult(stream as Stream);
+    }
+
+    private void ApllyGOST(DocX document)
+    {
+        document.MarginTop = 30;
+        document.MarginBottom = 0;
+        document.MarginLeft = 20;
+        document.MarginRight = 10;
+
+        foreach (var paragraph in document.Paragraphs)
+        {
+            paragraph.
+                Font("Times New Roman").
+                FontSize(14);
+
+            paragraph.Alignment = Alignment.both;
+            paragraph.LineSpacing = (float)15;
+        }
+        document.Save();
     }
 }
